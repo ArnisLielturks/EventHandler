26afef2c44182c3d0dc3914fb8b51640c401c83c
arnislielturks <arnis.lielturks@accenture.com>
2015-05-21 12:35:43 +0000

    some sort of comments (i tried)

--- src/main.cpp
+++ src/main.cpp
@@ -13,61 +13,76 @@ enum EventType {
 class Observer
 {
 public:
-   virtual void catchEvent(EventType e) = 0;
-   virtual ~Observer(){};
+    virtual void catchEvent(EventType e) = 0;
+    virtual ~Observer(){};
 };
 
 class Observed
 {
-   typedef vector<Observer*> ObserverList;
-   typedef map<EventType, ObserverList> observersTable;
-   observersTable m_observers;
+    //List of observers
+    typedef vector<Observer*> ObserverList;
+    typedef map<EventType, ObserverList> observersTable;
+    //map of observers for specific event type
+    observersTable m_observers;
 public:
-   void addObserver(Observer &observer, EventType eType) {
-      m_observers[eType].push_back(&observer);
-   }
+    //Add observer to the evtHandler
+    void addObserver(Observer &observer, EventType eType) {
+        m_observers[eType].push_back(&observer);
+    }
 
-   void sendEvent(EventType event) {
-      ObserverList& observers = m_observers[event];
-      for (ObserverList::iterator it = observers.begin(); it != observers.end(); ++it) {
-         Observer *observer = *it;
-         observer->catchEvent(event);
-      }
+    //Send out evt to registered observers for specific event
+    void sendEvent(EventType event) {
+        ObserverList& observers = m_observers[event];
+        for (ObserverList::iterator it = observers.begin(); it != observers.end(); ++it) {
+            Observer *observer = *it;
+            observer->catchEvent(event);
+        }
    }
 };
 
+//Create class that extends Observer class
 class Player: public Observer {
 public:
-   void catchEvent(EventType evt) {
-      cout << "player observer " << evt << endl;
-   }
+    //Override method to catch events
+    void catchEvent(EventType evt) {
+        cout << "player observer " << evt << endl;
+    }
 };
 
+//Second class for testing
 class SecondPlayer: public Observer {
-   void catchEvent(EventType evt) {
-      cout << "second player observer " << evt << endl;
-   }
+    //Override method to catch events
+    void catchEvent(EventType evt) {
+        cout << "second player observer " << evt << endl;
+    }
 };
 
 int main()
 {
-   Observed evtHandler;
-   Player p;
-   evtHandler.addObserver(p, EVENT_FIRST);
-   SecondPlayer p2;
-   evtHandler.addObserver(p2, EVENT_SECOND);
-   evtHandler.addObserver(p2, EVENT_FIRST);
+    //Create global event handler class
+    Observed evtHandler;
+    //Create test class
+    Player p;
+    //add test class to the observers list in the event handler class
+    evtHandler.addObserver(p, EVENT_FIRST);
+    //second test class
+    SecondPlayer p2;
+    //add second test class to observer list in the event handler class
+    evtHandler.addObserver(p2, EVENT_SECOND);
+    evtHandler.addObserver(p2, EVENT_FIRST);
 
    cout << "input event code: ";
    int evt;
    cin >> evt;
    switch(evt) {
-      case 0:
-         evtHandler.sendEvent(EVENT_FIRST);
-         break;
-      case 1:
-         evtHandler.sendEvent(EVENT_SECOND);
-         break;
+        case 0:
+            //Send event to eventHandler class
+            evtHandler.sendEvent(EVENT_FIRST);
+            break;
+        case 1:
+            //Send event to eventHandler class
+            evtHandler.sendEvent(EVENT_SECOND);
+            break;
    }
 
    return 0;
